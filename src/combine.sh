d60bcf420227949f16c0e463f9d09c2c507464ee
NQAP <oliverjheng@gmail.com>
2025-08-26 12:03:42 +0800

    Some file name change

--- src/combine.sh
+++ src/combine.sh
@@ -1,3 +1,3 @@
-python combine_train_test.py ./origin_dataset/UNSW_NB15_training-set.csv ./origin_dataset/UNSW_NB15_testing-set.csv -o ./extra_dataset/combined.csv --how rows
+python ./src/combine_train_test.py ./origin_dataset/UNSW_NB15_training-set.csv ./origin_dataset/UNSW_NB15_testing-set.csv -o ./extra_dataset/combined.csv --how rows
 
-python read_dataset.py ./extra_dataset/combined.csv
+python ./src/preprocessing.py ./extra_dataset/combined.csv
